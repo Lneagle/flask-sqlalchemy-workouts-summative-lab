898960aae2aa8fc47c4639f2e14b907753c9f666
Lacey Murphy <lneagle@gmail.com>
2025-09-19 10:15:53 -0400

    Added table constraints and model validations

--- server/models.py
+++ server/models.py
@@ -1,13 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy import CheckConstraint
 db = SQLAlchemy()
 
 class Exercise(db.Model):
     __tablename__ = 'exercises'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
     category = db.Column(db.String)
     equipment_needed = db.Column(db.Boolean)
 
@@ -15,12 +16,18 @@ class Exercise(db.Model):
 
     workouts = association_proxy('workout_exercises', 'workout', creator=lambda workout_obj: WorkoutExercise(workout=workout_obj))
 
+    @validates('category')
+    def validate_category(self, key, value):
+        if value not in ["Aerobic", "Strength", "Flexibility", "Balance", "Other"]:
+            raise ValueError('Category must be one of: "Aerobic", "Strength", "Flexibility", "Balance", "Other"')
+        return value
+
 class Workout(db.Model):
     __tablename__ = 'workouts'
 
     id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.Date)
-    duration_minutes = db.Column(db.Integer)
+    date = db.Column(db.Date, nullable=False)
+    duration_minutes = db.Column(db.Integer, db.CheckConstraint("duration_minutes < 1440")) # Constraint: duration is less than one day
     notes = db.Column(db.Text)
 
     workout_exercises = db.relationship('WorkoutExercise', back_populates='workout', cascade='all, delete-orphan')
@@ -40,3 +47,9 @@ class WorkoutExercise(db.Model):
 
     workout = db.relationship('Workout', back_populates='workout_exercises')
     exercise = db.relationship('Exercise', back_populates='workout_exercises')
+    
+    @validates('reps', 'sets', 'duration_seconds')
+    def validate_summary(self, key, value):
+        if value <= 0:
+            raise ValueError("Reps, sets, and duration must be greater than 0")
+        return value
